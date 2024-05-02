24ff1f91d6faebe4262e4c55ca239fa4a8fdf2c3
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2024-05-02 23:45:15 +0530

    Send data from tcp server

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,18 @@
-import * as net from "net";
+import * as net from "node:net";
 
 const server = net.createServer((socket) => {
-  socket.end();
-});
+  console.log("Client connected");
+  socket.on("data", (data) => {
+    console.log(data.toString());
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    socket.end();
+  });
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+  socket.on("end", () => {
+    console.log("Client disconnected");
+  });
+  // socket.end();
+});
 
 // Uncomment this to pass the first stage
 server.listen(4221, "localhost", () => {
