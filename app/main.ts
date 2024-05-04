b752289bbd3e54a16ea18c487a8b0464dd97e72d
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2024-05-04 12:00:17 +0530

    fix test case

--- app/main.ts
+++ app/main.ts
@@ -15,6 +15,8 @@ const server = net.createServer((socket) => {
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\nContent-Length: ${echoStr.length}\n\r\n${echoStr}`
       );
+    } else {
+      socket.write("HTTP/1.1 404 OK\r\n\r\n");
     }
     socket.end();
   });
